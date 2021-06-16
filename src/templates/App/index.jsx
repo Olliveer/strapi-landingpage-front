e131a2703c93475a20f2e04aa4375928b90604d1
José Oliveira <j.oliveira90@live.com>
2021-06-16 16:15:59 -0300

    initial commit

---
+++ src/templates/App/index.jsx
@@ -0,0 +1,13 @@
+import { Wrapper } from './styles';
+
+function Home() {
+  return (
+    <div>
+      <Wrapper>
+        <h1>Hellow</h1>
+      </Wrapper>
+    </div>
+  );
+}
+
+export default Home;
